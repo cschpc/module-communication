a5f154754597605a943df85af2da07f91e82dd79
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2020-12-18 11:19:02 +0200

    Check API with custom context exit

--- workflow/prefect/api-matching.py
+++ workflow/prefect/api-matching.py
@@ -3,6 +3,23 @@ import inspect
 
 # API matching with type annotations
 
+# Custom flow checking APIs
+class myFlow(Flow):
+
+    def __exit__(self, _type, _value, _tb):
+        super().__exit__(_type, _value, _tb)
+        # Check API matching
+        for task in flow:
+            upstream = flow.upstream_tasks(task)
+            if upstream:
+                # How to deal with multiple upstream tasks ?
+                upstream = upstream.pop()
+                out_sig = inspect.signature(upstream.run)
+                in_sig = inspect.signature(task.run)
+                match = [param.annotation for param in in_sig.parameters.values()][0] == out_sig.return_annotation
+                print("Matching of task {} to {} : {} ".format(upstream, task, match))
+
+
 @task
 def task1(a : int) -> float:
     return a
@@ -15,19 +32,8 @@ def task2(b : int) -> float:
 def task3(c : float) -> float:
     return c
 
-with Flow('api-test') as flow:
+with myFlow('api-test') as flow:
     r1 = task1(4)
     r2 = task2(r1)
     r3 = task3(r2)
-
-# Check API matching
-for task in flow:
-    upstream = flow.upstream_tasks(task)
-    if upstream:
-        # How to deal with multiple upstream tasks ?
-        upstream = upstream.pop()
-        out_sig = inspect.signature(upstream.run)
-        in_sig = inspect.signature(task.run)
-        match = [param.annotation for param in in_sig.parameters.values()][0] == out_sig.return_annotation
-        print("Matching of task {} to {} : {} ".format(upstream, task, match))
 
--- workflow/prefect/api-matching2.py
+++ workflow/prefect/api-matching2.py
@@ -1,8 +1,21 @@
 from prefect import task, Task, Flow
 import inspect
 
-
 # API matching with explicit methods
+
+# Custom flow checking APIs
+class myFlow(Flow):
+
+    def __exit__(self, _type, _value, _tb):
+        super().__exit__(_type, _value, _tb)
+        # Check API matching
+        for task in flow:
+            upstream = flow.upstream_tasks(task)
+            if upstream:
+                # How to deal with multiple upstream tasks ?
+                upstream = upstream.pop()
+                match = task.input_api() == upstream.output_api()
+                print("Matching of task {} to {} : {} ".format(upstream, task, match))
 
 class MyTask1(Task):
 
@@ -42,18 +55,9 @@ task1 = MyTask1()
 task2 = MyTask2()
 task3 = MyTask3()
 
-with Flow('api-test') as flow:
+with myFlow('api-test') as flow:
     r1 = task1(4)
     r2 = task2(r1)
     r3 = task3(r2)
-
-# Check API matching
-for task in flow:
-    upstream = flow.upstream_tasks(task)
-    if upstream:
-        # How to deal with multiple upstream tasks ?
-        upstream = upstream.pop()
-        match = task.input_api() == upstream.output_api()
-        print("Matching of task {} to {} : {} ".format(upstream, task, match))
 
 
